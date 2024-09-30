3bd7216222c4cbb92b60779fe5a2f78b09055912
ErickIgles <erick.almeida2125@gmail.com>
2024-09-30 13:14:11 -0300

    Adicionando o app usuarios, boostrap4 e stdimage ao INSTALLED_APPS

--- aluguel_de_carros/settings.py
+++ aluguel_de_carros/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuarios',
+    'bootstrap5',
+    'stdimage',
 ]
 
 MIDDLEWARE = [
