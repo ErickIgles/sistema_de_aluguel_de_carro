44061bcc86b28b788854e0a606c969f229d4aadd
ErickIgles <erick.almeida2125@gmail.com>
2024-10-02 16:09:13 -0300

    Editando rota da url index para home

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import index, criar_usuario, login_page
+from .views import home, criar_usuario, login_page, logout_page
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', home, name='home'),
     path('form_usuario/', criar_usuario, name='criar_usuario'),
     path('login_page/', login_page, name='login_page'),
+    path('logout_page/', logout_page, name='logout_page'),
 ]
