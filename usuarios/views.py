0c81c64e4cd9953ea46a38a0c09796733290ff2b
ErickIgles <erick.almeida2125@gmail.com>
2024-10-02 20:42:25 -0300

    Adicionando view function para atualizar dados do usuário e senha

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from .forms import CustomUserCreateForm, CustomLoginPage
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from .forms import CustomUserCreateForm, CustomLoginPage, CustomUserChangeForm, PasswordUserChange
 from .models import CustomUser
 # Create your views here.
 
@@ -56,6 +56,44 @@ def login_page(request):
         form = CustomLoginPage()
     
     return render(request, 'login_page.html', {'form': form})
+
+
+def atualizar_usuario(request):
+
+    form = CustomUserChangeForm(instance=request.user)
+
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+
+            try:
+                form.save()
+                return redirect('perfil_page')
+            except ValueError as err:
+                messages.error(request, f'Erro ao atualizar os dados: {err}!')
+                return redirect('perfil_page')
+    return render(request, 'atualizar_usuario.html', {'form':form})
+
+
+def atualizar_senha(request):
+
+    form = PasswordUserChange(user=request.user)
+
+    if request.method == 'POST':
+        form = PasswordUserChange(user=request.user, data=request.POST)
+        
+        if form.is_valid():
+
+            try:
+                user = form.save()
+                update_session_auth_hash(request, user)
+                return redirect('perfil_page')
+            except ValueError as err:
+                messages.error(request, f'Erro ao atualizar a senha {err}')
+                return redirect('perfil_page')
+    
+    return render(request, 'atualizar_senha.html', {'form': form})
 
 
 def logout_page(request):
