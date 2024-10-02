2abf88c94523233e8397a789463df9749e860bd3
ErickIgles <erick.almeida2125@gmail.com>
2024-10-02 18:45:41 -0300

    Adicionando adicionando rota o  perfil

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import home, criar_usuario, login_page, logout_page
+from .views import home, criar_usuario, login_page, logout_page, perfil_page
 
 urlpatterns = [
     path('', home, name='home'),
     path('form_usuario/', criar_usuario, name='criar_usuario'),
     path('login_page/', login_page, name='login_page'),
     path('logout_page/', logout_page, name='logout_page'),
+    path('perfil_page/', perfil_page, name='perfil_page'),
 ]
