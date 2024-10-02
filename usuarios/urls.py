77e546c3fca893638c7fdbfa29993c8c5c1f039e
ErickIgles <erick.almeida2125@gmail.com>
2024-10-02 13:53:51 -0300

    Adicionando rota para formulário de criação de usuários

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import index
+from .views import index, criar_usuario
 
 urlpatterns = [
     path('', index, name='index'),
+    path('form_usuario/', criar_usuario, name='criar_usuario'),
 ]
