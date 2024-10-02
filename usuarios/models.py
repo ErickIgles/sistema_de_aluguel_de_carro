61ee695a8ff452889f974dffe9fe599a33ca329e
ErickIgles <erick.almeida2125@gmail.com>
2024-10-02 10:30:38 -0300

    Criando o modelo personalizado de usuário

--- usuarios/models.py
+++ usuarios/models.py
@@ -12,7 +12,7 @@ class UserManager(BaseUserManager):
     def _create_user(self, email, password, **extra_fields):
 
         if not email:
-            raise ValueError('Email is required')
+            raise ValueError('É necessário informar um e-mail!')
         email = self.normalize_email(email) # realiza a validação se p valor informado é um email verdadeiro.
         user = self.model(email=email, username=email, **extra_fields) 
         user.set_password(password) # criptografa a semha.
@@ -30,21 +30,21 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
 
         if extra_fields.get('is_staff') is not True:
-            raise ValueError('is_staff must be True')
+            raise ValueError('is_staff precisa ser True')
        
         if extra_fields.get('is_superuser') is not True:
-            raise ValueError('is_superuser must be True')
+            raise ValueError('is_superuser precisa ser True')
         
         return self._create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
-    email = models.EmailField('E-mail', unique=True)
-    first_name = models.CharField('first_name', max_length=200)
-    last_name = models.CharField('last_name', max_length=200)
+    email = models.EmailField('e-mail', unique=True)
+    nome = models.CharField('nome', max_length=200)
+    sobrenome = models.CharField('sobrenome', max_length=200)
     cpf = models.CharField('cpf', max_length=11)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = ['nome', 'sobrenome']
 
     def __str__(self):
         return self.email
