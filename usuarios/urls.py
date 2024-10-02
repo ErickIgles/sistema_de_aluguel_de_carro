c9a6ccca46fc0feafbf3bfa87fdc747f6c9e4285
ErickIgles <erick.almeida2125@gmail.com>
2024-10-02 20:39:11 -0300

    Adicionando rota para atualização de dados e senha

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import home, criar_usuario, login_page, logout_page, perfil_page
+from .views import home, criar_usuario, login_page, logout_page, perfil_page, atualizar_usuario, atualizar_senha
 
 urlpatterns = [
     path('', home, name='home'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login_page/', login_page, name='login_page'),
     path('logout_page/', logout_page, name='logout_page'),
     path('perfil_page/', perfil_page, name='perfil_page'),
+    path('atualizar_dados/', atualizar_usuario, name='atualizar_usuario'),
+    path('atualizar_senha/', atualizar_senha, name='atualizar_senha'),
 ]
