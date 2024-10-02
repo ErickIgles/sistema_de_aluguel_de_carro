c769b90dd7b2ea86c2e2c1d00a917c156df9c41a
ErickIgles <erick.almeida2125@gmail.com>
2024-10-02 15:57:42 -0300

    Adicionando rota de login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import index, criar_usuario
+from .views import index, criar_usuario, login_page
 
 urlpatterns = [
     path('', index, name='index'),
     path('form_usuario/', criar_usuario, name='criar_usuario'),
+    path('login_page/', login_page, name='login_page'),
 ]
