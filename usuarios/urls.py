3b3d35f49b516bb1316bc525c992998077acb83a
ErickIgles <erick.almeida2125@gmail.com>
2024-10-02 21:12:40 -0300

    Adicionando rota para deletar conta

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import home, criar_usuario, login_page, logout_page, perfil_page, atualizar_usuario, atualizar_senha
+from .views import home, criar_usuario, login_page, logout_page, perfil_page, atualizar_usuario, atualizar_senha, deletar_usuario
 
 urlpatterns = [
     path('', home, name='home'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('perfil_page/', perfil_page, name='perfil_page'),
     path('atualizar_dados/', atualizar_usuario, name='atualizar_usuario'),
     path('atualizar_senha/', atualizar_senha, name='atualizar_senha'),
+    path('deletar_conta/', deletar_usuario, name='deletar_usuario'),
 ]
