c9517824b144e0a00aa178dad1e87b8b8ae85d3e
ErickIgles <erick.almeida2125@gmail.com>
2024-10-17 21:51:25 -0300

    Adicionando modelo carros ao painel administrativo do Django

--- carros/admin.py
+++ carros/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Carros
 # Register your models here.
+
+@admin.register(Carros)
+class CustomCar(admin.ModelAdmin):
+    list_display = ('placa', 'marca', 'modelo', 'ano', 'cor', 'quilometragem', 'valor_diario')
+
